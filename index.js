a2ca4b595ed3481e0a27aa50c7d472a4d94e2cb5
Wymen Soo Thin Zhun <49495543-pizzacap20@users.noreply.replit.com>
2025-12-24 22:00:59 +0000

    optional and compulsory done

--- index.js
+++ index.js
@@ -27,86 +27,227 @@ async function getPostgresVersion() {
 
 getPostgresVersion();
 
+// Post a single posts with user_id
+// I want to make a posts, that has user_id
 app.post("/posts", async (req, res) => {
-  const { title, content, user_id } = req.body;
   const client = await pool.connect();
+  const { title, content, user_id } = req.body;
   try {
-    // Check if user exits
+    // check if user exists
     const userExists = await client.query(
       "SELECT id FROM users WHERE id = $1",
       [user_id],
     );
     if (userExists.rows.length > 0) {
-      // User exists, add post
+      // user exists, add post
       const post = await client.query(
         "INSERT INTO posts (title, content, user_id, created_at) VALUES ($1, $2, $3, CURRENT_TIMESTAMP) RETURNING *",
         [title, content, user_id],
       );
-      // Send new post data back to client
+      // send new data post back to the client
       res.json(post.rows[0]);
     } else {
-      // User does not exist
-      res.status(400).json({ error: "User does not exist" });
+      // user does not exist
+      res.status(404).json({ error: "User does not exist" });
     }
-  } catch (err) {
-    console.log(err.stack);
-    res
-      .status(500)
-      .json({ error: "Something went wrong, please try again later!" });
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).json({ error: "An error occurred, please try again." });
   } finally {
     client.release();
   }
 });
 
-// Adding a like to a post
+// INSERT INTO likes (user_id, post_id, created_at)
+// VALUES (1, 2, CURRENT_TIMESTAMP)
+// post a single likes with user_id and post_id
 app.post("/likes", async (req, res) => {
-  const { user_id, post_id } = req.body
-  
-  const client = await pool.connect()
-  
+  const client = await pool.connect();
+  const { user_id, post_id } = req.body;
   try {
-    const newLike = await client.query("INSERT INTO likes (user_id, post_id, created_at) VALUES ($1, $2, CURRENT_TIMESTAMP) RETURNING *", [user_id, post_id])
-    
-    res.json(newLike.rows[0])
+    const newLike = await client.query(
+      "INSERT INTO likes (user_id, post_id, created_at) VALUES ($1, $2, CURRENT_TIMESTAMP) RETURNING *",
+      [user_id, post_id],
+    );
+    res.json(newLike.rows[0]);
   } catch (error) {
-    console.log(error.stack)
-    res.status(500).send("An error occurred, please try again.")
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
   } finally {
-    client.release()
+    client.release();
   }
-})
+});
 
-// Delete like from a post
+// DELETE FROM likes WHERE id = 2
+// delete a single like using id
 app.delete("/likes/:id", async (req, res) => {
+  const client = await pool.connect();
   const { id } = req.params;
-
-  const client = await pool.connect()
-
   try {
-    await client.query("DELETE FROM likes WHERE id = $1", [id])
-    res.json({ message: "Like Deleted Successfully" })
-  } catch (err) {
-    console.log(err.stack)
-    res.status(500).send("An error occurred, please try again")
+    await client.query("DELETE FROM likes WHERE id = $1", [id]);
+    res.json({ message: "like deleted successfully" });
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
   } finally {
-    client.release()
+    client.release();
   }
-})
+});
 
+// SELECT users.username
+// FROM likes
+// INNER JOIN users
+// ON likes.user_id = users.id
+// WHERE likes.post_id = 1
+// get all the likes in a post using post_id
+// transform result into an array of usernames
 app.get("/likes/post/:post_id", async (req, res) => {
-  const {post_id} = req.params;
-  const client = await pool.connect()
+  const client = await pool.connect();
+  const { post_id } = req.params;
   try {
-    // Fetch all likes for the specific post
-    const likes = await client.query("SELECT users.username FROM likes INNER JOIN users ON likes.user_id = users.id WHERE likes.post_id = $1", [post_id])
-  res.json(likes.rows)
-  } catch (err) {
-    console.error(err.stack)
-    res.status(500).send("An error occurred, please try again.")
+    const likes = await client.query(
+      "Select users.username FROM likes INNER JOIN users ON likes.user_id = users.id WHERE likes.post_id = $1",
+      [post_id],
+    );
+    // array of string usernames
+    const result = likes.rows.map((row) => row.username);
+    res.json(result);
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
   } finally {
-    client.release()
+    client.release();
   }
-})
+});
+
+// UPDATE posts
+// SET views = views + 1
+// WHERE id = 1
+// increment 1 view count every time it's accessed using id
+app.get("/posts/:id", async (req, res) => {
+  const client = await pool.connect();
+  const { id } = req.params;
+  try {
+    const query = await client.query(
+      "UPDATE posts SET views = views + 1 WHERE id = $1 RETURNING *",
+      [id],
+    );
+    if (query.rowCount === 0) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res.json(query.rows[0]);
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
+  }
+});
+
+// INSERT INTO comment (user_id, post_id, content, created_at) VALUES
+// (4, 3, 'cool this is sick!!!', CURRENT_TIMESTAMP)
+// post a single comment with user_id and post_id and content
+app.post("/comment", async (req, res) => {
+  const client = await pool.connect();
+  const { user_id, post_id, content } = req.body;
+  try {
+    const result = await client.query(
+      "INSERT INTO comment (user_id, post_id, content, created_at) VALUES ($1, $2, $3, CURRENT_TIMESTAMP) RETURNING *",
+      [user_id, post_id, content],
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
+  } finally {
+    client.release();
+  }
+});
+
+// SELECT comment.id, users.username, comment.content, comment.created_at
+// FROM comment
+// JOIN users ON comment.user_id = users.id
+// WHERE comment.post_id = 1
+// get all the comment for a specific post, including id, content, users.username and timestamp
+app.get("/comment/post/:id", async (req, res) => {
+  const client = await pool.connect();
+  const { id } = req.params;
+  try {
+    const result = await client.query(
+      "SELECT comment.id, users.username, comment.content, comment.created_at FROM comment JOIN users ON comment.user_id = users.id WHERE comment.post_id = $1",
+      [id],
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
+  } finally {
+    client.release();
+  }
+});
+
+// UPDATE comment
+// SET content = 'this is way better than cool!!!!'
+// WHERE id = 1
+// Edit the content of a comment
+app.put("/comment/:id", async (req, res) => {
+  const client = await pool.connect();
+  const { content, id } = req.body;
+  try {
+    const result = await client.query(
+      "UPDATE comment SET content = $1 WHERE id = $2 RETURNING *",
+      [content, id],
+    );
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "id not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
+  } finally {
+    client.release();
+  }
+});
+
+// DELETE FROM comment WHERE id = 3
+// Delete a comment by its id
+app.delete("/comment/:id", async (req, res) => {
+  const client = await pool.connect();
+  const { id } = req.params;
+  try {
+    const result = await client.query("DELETE FROM comment WHERE id = $1", [
+      id,
+    ]);
+    if (result.rowCount === 0) {
+      return res.status(404).send("comment not found");
+    }
+    res.json({ message: "Comment deleted successfully." });
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
+  } finally {
+    client.release();
+  }
+});
+
+// INSERT INTO comment_likes (comment_id, user_id)
+// VALUES (1, 4)
+// add a comment_like using comment_id and user_id
+app.post("/comment/:id/like", async (req, res) => {
+  const client = await pool.connect();
+  const { comment_id, user_id } = req.body;
+  try {
+    const result = await client.query(
+      "INSERT INTO comment_likes (comment_id, user_id) VALUES ($1, $2) RETURNING *",
+      [comment_id, user_id],
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).send("An error occurred, please try again.");
+  } finally {
+    client.release();
+  }
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the twitter API!" });
